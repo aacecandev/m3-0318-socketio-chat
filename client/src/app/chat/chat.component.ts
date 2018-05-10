ae51cd82db718d08453157a10a2483c42ace5200
Marc Pomar <marc@faable.com>
2018-05-10 17:48:45 +0200

    socketio

---
+++ client/src/app/chat/chat.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'services/chat.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  textbox:string;
+
+  constructor(private chatService:ChatService){
+  }
+
+  ngOnInit() {
+  }
+
+  clickSend(){
+    this.chatService.sendMessage(this.textbox);
+    this.textbox = "";
+  }
+
+}
